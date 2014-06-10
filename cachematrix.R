021555ac6eb26d351e1e28aead8d399e5782ce3b
Christian L. Schultz <clschultz@gmail.com>
2014-06-10 11:45:22 -0700

    added initial comments to cachematrix.R and rendered README.html

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions facilitate calculation of the inverse of a square
+## matrix.  If the value has already been calculated the resulting inversion
+## matrix is pulled from a cache store.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix - creates a special matrix with a list of functions to:
+## 1) set the value of the matrix
+## 2) get the value of the matrix
+## 3) set the value of the inverse matrix
+## 4) get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - returns the inverse of square matrix.  If the inverse has
+## already been calculated it is pulled from the cache store
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
