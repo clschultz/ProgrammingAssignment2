9a568790bcecf5b7a4fa9ce2739bc82081653ecd
Christian L. Schultz <clschultz@gmail.com>
2014-06-10 12:19:55 -0700

    skeleton code added, fixed comments to show assumption of invertable matrix (and therefor a square matrix)

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
-## The following functions facilitate calculation of the inverse of a square
-## matrix.  If the value has already been calculated the resulting inversion
-## matrix is pulled from a cache store.
+## The following functions facilitate calculation of the inverse of a square 
+## matrix.  If the value has already been calculated the resulting inversion 
+## matrix is pulled from a cache store.  This function assumes that the matrix
+## is always invertable, and by this assumption, the matrix is square
 
 
 ## makeCacheMatrix - creates a special matrix with a list of functions to:
@@ -10,7 +11,17 @@
 ## 4) get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) m <<- solve
+    getSolve <- function() m
+    list(set = set, get = get, 
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
@@ -18,5 +29,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## already been calculated it is pulled from the cache store
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getSolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m)
+    m
 }
